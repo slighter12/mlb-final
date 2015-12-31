b2d316d9fb8e341a2718d39e4feac52cb1e735b0
hohohahalala <hohohahalala1223@gmail.com>
2015-12-31 11:31:02 +0800

    0.4

--- ml.sh
+++ ml.sh
@@ -3,5 +3,8 @@
 # svm-predict -b 1 ./output/test.scale ./output/train-result ./result/svm-test-result
 
 ## rvkde
-./bin/rvkde --cv --best --classify -n 5 -v ./output/train.scale -b 1,10,1 --ks 1,100,1 --kt 1,100,1 --f-measmure > ./output/rvkde-train-result
-# ./bin/rvkde --best --predict --classify -v ./output/train.scale -V ./output/test.scale -b 1 --ks 11 --kt 23 > ./result/rvkde-test-result
+# ./bin/rvkde --cv --best --classify -n 5 -v ./output/train.scale -b 1,10,1 --ks 1,100,1 --kt 1,100,1 --f-measure > ./output/rvkde-train-result
+# ./bin/rvkde --best --predict --classify -v ./output/train.scale -V ./output/test.scale -b 6 --ks 90 --kt 90 > ./result/rvkde-test-result
+./bin/rvkde --best --predict --classify -v ./output/train.scale -V ./output/test.scale -b 1 --ks 93 --kt 48 > ./result/rvkde-test-result
+
+
